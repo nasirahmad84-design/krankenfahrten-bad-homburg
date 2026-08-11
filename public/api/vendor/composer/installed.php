adeb62d02cae3c827384a38a83885c05fa9a0286
Nasir Ahmad <nasirahmad@mbp-lyp60m7k5f.fritz.box>
2026-08-11 11:14:59 +0200

    chore: update composer installation metadata

--- public/api/vendor/composer/installed.php
+++ public/api/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'krankenfahrten-bad-homburg/form-handler',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '1f404651a18f1672bb8de46cfa63bbb1e0e8e27e',
+        'reference' => '375f3fac3e1b7d6d40a2cec7a4224a0059fd3500',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'krankenfahrten-bad-homburg/form-handler' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '1f404651a18f1672bb8de46cfa63bbb1e0e8e27e',
+            'reference' => '375f3fac3e1b7d6d40a2cec7a4224a0059fd3500',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../../../',
             'aliases' => array(),
